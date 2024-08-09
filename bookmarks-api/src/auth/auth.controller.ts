2c707cf75db7ef1de3d790fd24e1489d148d37e3
Shalom Habtamu <shalomhabtamum@gmail.com>
2024-08-09 22:47:55 +0300

    signup logic

--- bookmarks-api/src/auth/auth.controller.ts
+++ bookmarks-api/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, ParseIntPipe, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 
@@ -7,9 +7,8 @@ export class AuthController {
     constructor(private authService: AuthService ) {}
      
     @Post('signup')
-    signup(
-        @Body() dto: AuthDto){
-        return this.authService.signup();
+    signup(@Body() dto: AuthDto){        
+        return this.authService.signup(dto);
     }
 
     @Post('signin')
--- bookmarks-api/src/main.ts
+++ bookmarks-api/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true//filters so that unnecessary things aren't logged
+  }));
   await app.listen(3333);
 }
 bootstrap();
